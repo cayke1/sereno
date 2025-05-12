06021c0bbb69f6d434b736663190c86da5fa7efa
Cayke Daniel <contato.cayke@gmail.com>
2025-05-12 20:29:20 -0300

    feat: add patient info and actions to document

--- src/components/documents/DocumentFolder.tsx
+++ src/components/documents/DocumentFolder.tsx
@@ -16,6 +16,8 @@ import {
 } from "@/components/ui/table";
 import { FileText } from "lucide-react";
 import { formatDate } from "@/lib/formatDate";
+import { Button } from "../ui/button";
+import { toast } from "sonner";
 
 interface Document {
   id: string;
@@ -23,7 +25,8 @@ interface Document {
   mimeType: string;
   size?: string;
   createdAt: string;
-  owner_id?: string;
+  owner?: any;
+  url: string;
 }
 
 interface DocumentFolderProps {
@@ -39,6 +42,22 @@ export function DocumentFolder({
   documents,
   showPatientColumn = false,
 }: DocumentFolderProps) {
+  const handleDelete = async (id: string) => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/document/${id}`;
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    });
+
+    if (!response.ok) {
+      toast.error("Falha ao excluir o documento");
+      return;
+    }
+
+    toast.success("Documento excluído com sucesso");
+  };
   return (
     <Card>
       <CardHeader>
@@ -58,6 +77,7 @@ export function DocumentFolder({
                 {showPatientColumn && <TableHead>Paciente</TableHead>}
                 <TableHead>Tamanho</TableHead>
                 <TableHead>Atualizado</TableHead>
+                <TableHead className="text-right">Ações</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
@@ -69,12 +89,39 @@ export function DocumentFolder({
                   <TableCell className="font-medium">
                     <div className="flex items-center gap-2">
                       <FileText size={18} className="text-mint-600" />
-                      <span className="truncate max-w-[300px]">{doc.filename}</span>
+                      <span className="truncate max-w-[300px]">
+                        {doc.filename}
+                      </span>
                     </div>
                   </TableCell>
-                  {showPatientColumn && <TableCell>{doc.owner_id}</TableCell>}
+                  {showPatientColumn && doc.owner ? (
+                    <TableCell>{doc.owner.name}</TableCell>
+                  ) : (
+                    <TableCell> </TableCell>
+                  )}
                   <TableCell>{doc.size}</TableCell>
                   <TableCell>{formatDate(doc.createdAt)}</TableCell>
+
+                  <TableCell>
+                    <div className="flex flex-col gap-2 justify-end">
+                      <a
+                        href={`${doc.url}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-mint-600 hover:text-mint-800"
+                        download={doc.filename}
+                      >
+                        Baixar
+                      </a>
+
+                      <a
+                        className="text-red-600 hover:text-red-800"
+                        onClick={() => handleDelete(doc.id)}
+                      >
+                        Excluir
+                      </a>
+                    </div>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
